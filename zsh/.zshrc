fbb31ddda6737085b845dea8c7fc26935c45138d
jackh <jackhuang1205@gmail.com>
2023-03-12 12:17:28 +0800

    Add zsh settings

---
+++ zsh/.zshrc
@@ -0,0 +1,33 @@
+# basic settings
+export TZ='Asia/Taipei'
+export TERM="xterm-256color"
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+# oh-my-zsh settins
+ZSH_THEME="powerlevel10k/powerlevel10k"
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+export ZSH="$HOME/.oh-my-zsh"
+plugins=(
+    git
+    vi-mode
+    history
+    colored-man-pages
+    colorize
+    web-search
+    dirhistory
+    extract
+
+    # other plugins
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# bind autocompelet
+bindkey '^O' autosuggest-accept	
+# update option
+DISABLE_AUTO_UPDATE="true"
---
+++ zsh/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+check_install () {
+    for exe in "$@"; do
+        if ! command -v $exe &> /dev/null; then
+            sudo apt-get install -y $exe
+        fi
+    done
+}
+
+check_install git zsh wget curl powerline autojump
+
+# install .oh-my-zsh
+if [ ! -d ${HOME}/.oh-my-zsh ]; then
+    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /tmp/install.sh
+    sh /tmp/install.sh
+fi
+
+# install zsh-syntax-highlight
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+# install zsh-autosuggestions
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+# install p10k theme
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/theme/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+ln -sf `pwd`/.zshrc ~/.zshrc
+
+sudo chsh -s $(which zsh)
