0d999a865d813e76b90ba8e9e92404af0d86dabd
jack <jackhuang1205@gmail.com>
2024-12-30 02:21:43 +0800

    Add comments

--- zsh/scripts/llvm.zsh
+++ zsh/scripts/llvm.zsh
@@ -10,20 +10,7 @@ my_git_name=main
 llvm_git_name=llvm_project_main
 llvm_url="https://github.com/llvm/llvm-project.git"
 
-
-_llvm_git_init () {
-  _show_and_run git remote add $llvm_git_name $llvm_url
-}
-
-_llvm_git_remove () {
-  _show_and_run git remote remove $llvm_git_name
-}
-
-_llvm_is_git_init () {
-  git remote | grep $llvm_git_name > /dev/null
-  return $?
-}
-
+# llvm_git_update: Sync with the officail branch {{{
 llvm_git_update () {
   if _llvm_is_git_init; then
     # nothing
@@ -36,8 +23,31 @@ llvm_git_update () {
   _show_and_run git merge $llvm_git_name/main
   _show_and_run git push
 }
+# }}} llvm_git_update
 
+# llvm_format: Fomrat the code {{{
 llvm_format () {
   _show_and_run git clang-format HEAD~1
   _show_and_run git commit --amend -a
 }
+# }}} llvm_format
+
+# Basic function
+# _llvm_git_init {{{
+_llvm_git_init () {
+  _show_and_run git remote add $llvm_git_name $llvm_url
+}
+# }}} _llvm_is_git_init
+
+# _llvm_git_remove {{{
+_llvm_git_remove () {
+  _show_and_run git remote remove $llvm_git_name
+}
+# }}} _llvm_git_remove
+
+# _llvm_is_git_init {{{
+_llvm_is_git_init () {
+  git remote | grep $llvm_git_name > /dev/null
+  return $?
+}
+# }}} _llvm_is_git_init
