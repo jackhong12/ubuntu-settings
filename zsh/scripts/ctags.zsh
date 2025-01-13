3acabb974dc5d93de0cb0ca835d7a50a513d76d1
jackhong12 <jackhuang1205@gmail.com>
2025-01-13 11:38:05 +0800

    Generate tags of YACC and LEX files
    Fixes: #3

--- zsh/scripts/ctags.zsh
+++ zsh/scripts/ctags.zsh
@@ -4,7 +4,7 @@
 # ctags_gen_c: Generate tags for c code.
 
 ctags_gen_c () {
-  ctags -R -h ".h.hpp.p" --c++-kinds=+p $@
+  ctags -R -h ".h.hpp.p" --c++-kinds=+p --languages=C,C++ --langmap=C:.l.y.c $@
 }
 
 # }}} ctags
