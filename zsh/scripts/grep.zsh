083c35d40fe346f0b3dd3d9a704d2a2b57e6494f
jackhong12 <jackhuang1205@gmail.com>
2025-01-15 17:21:15 +0800

    Add grepr

--- zsh/scripts/grep.zsh
+++ zsh/scripts/grep.zsh
@@ -16,4 +16,13 @@ grepex () {
     $@
 }
 
-# }}}
+# }}} grepex
+
+# grepr: Grep recursively and solve the links automatically {{{
+grepr () {
+  for f in *; do
+    realf=$(realpath $f)
+    prun grep -rn $@ $realf
+  done
+}
+# }}} grepr
