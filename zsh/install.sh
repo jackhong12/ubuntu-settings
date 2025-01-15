3c08ca41b9be725f49c55f23408708ea13cbc95b
jackhong12 <jackhuang1205@gmail.com>
2025-01-15 16:46:26 +0800

    Use prun and check_install instead

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,33 +1,33 @@
-#!/bin/bash
+#!/bin/zsh
 
 source ../tools/utils.zsh
 
-_show_and_run check-install git zsh wget curl powerline autojump locales
+prun check_install git zsh wget curl powerline autojump locales
 
 # gen en_US.UTF-8
-_show_and_run sudo locale-gen en_US.UTF-8
+prun sudo locale-gen en_US.UTF-8
 
 # install .oh-my-zsh
 if [ ! -d ${HOME}/.oh-my-zsh ]; then
-    _show_and_run wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /tmp/install.sh
-    _show_and_run sh /tmp/install.sh
+    prun wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /tmp/install.sh
+    prun sh /tmp/install.sh
 fi
 
 # install zsh-syntax-highlight
 if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
-    _show_and_run git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    prun git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 # install zsh-autosuggestions
 if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
-    _show_and_run git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    prun git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 # install p10k theme
 if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/theme/powerlevel10k ]; then
-    _show_and_run git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    prun git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
-_show_and_run ln -sf `pwd`/.zshrc ~/.zshrc
+prun ln -sf `pwd`/.zshrc ~/.zshrc
 
-_show_and_run sudo chsh -s $(which zsh)
+prun sudo chsh -s $(which zsh)
