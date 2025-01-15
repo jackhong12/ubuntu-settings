09f16c743323cb100bebb5aa8d643913a8dded34
jackhong12 <jackhuang1205@gmail.com>
2025-01-15 13:43:15 +0800

    Add ctags_gen_zsh

--- zsh/scripts/ctags.zsh
+++ zsh/scripts/ctags.zsh
@@ -1,10 +1,24 @@
 #!/bin/zsh
 
-#> ctags: Macros for ctags {{{
-# ctags_gen_c: Generate tags for c code.
+# include utils.zsh {{{
+if [ -f ~/.zsh/utils.zsh ]; then
+  source ~/.zsh/utils.zsh
+else
+  printf "\033[0;31mNot file ~/.zsh/utils.zsh\033[0m\n"
+fi
+# }}} include utils.zsh
+
+# ctags_gen_c: Generate tags for c code {{{
 
 ctags_gen_c () {
-  ctags -R -h ".h.hpp.p" --c++-kinds=+p --languages=C,C++ --langmap=C:.l.y.c $@
+  _show_and_run ctags -R -h ".h.hpp.p" --c++-kinds=+p --languages=C,C++ --langmap=C:.l.y.c $@
 }
 
-# }}} ctags
+# }}} ctags_gen_c
+
+# ctags_gen_zsh: Generate tags for zsh shell {{{
+ctags_gen_zsh () {
+  _show_and_run ctags -R --languages=Sh --langmap=Sh:.zsh --extras=+q -f ~/.zsh/tags ~/.zsh
+}
+
+# }}} ctags_gen_zsh
