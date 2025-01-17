3d644868195b954375151ee3403ab2d96668f272
jackhong12 <jackhuang1205@gmail.com>
2025-01-17 11:04:56 +0800

    Exclude lex and swp files for grep

--- zsh/scripts/grep.zsh
+++ zsh/scripts/grep.zsh
@@ -12,12 +12,27 @@ fi
 grepex () {
   yacc_opt=""
   for f in $(find -name "*Parse.c"); do
-    yacc_opt+=" --exclude=$f"
+    yacc_opt+=" --exclude=$(basename -- $f)"
+  done
+
+  lex_opt=""
+  for f in $(find -name "*Lex.c"); do
+    lex_opt+=" --exclude=$(basename -- $f)"
+  done
+
+  swp_opt=""
+  for f in $(find -name "*.swp"); do
+    swp_opt+=" --exclude=$(basename -- $f)"
   done
 
   prun grep \
     --exclude=tags \
     $yacc_opt \
+    $lex_opt \
+    $swp_opt \
+    --exclude=nettran.log \
+    --exclude=temp_in \
+    --exclude=errors \
     $@
 }
 
