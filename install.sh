6216269052bad170baae205c56838a81852b2759
Jack Huang <jackhuang1205@gmail.com>
2025-01-28 23:06:00 +0800

    Update apt-get before installing and set git editor

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install zsh -y
+
 distribution=`lsb_release -a 2> /dev/null | grep Description | sed 's|Description:[\t ]*||g'`
 
 source ./zsh/scripts/utils.zsh
@@ -22,3 +25,7 @@ install tmux
 install fonts
 #install tools
 #install tools/zsh-help
+
+# git settings
+prun check_install git
+prun git config --global core.editor "vim"
