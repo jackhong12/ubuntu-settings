f47d45e7b04a2f04ef570cb2d51f56a95c086959
Jack Huang <jackhuang1205@gmail.com>
2025-06-06 10:45:46 +0800

    Add perf.zsh

---
+++ zsh/scripts/perf.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Setup perf in WSL2:
+#   sudo apt-get install flex bison
+#   git clone https://github.com/microsoft/WSL2-Linux-Kernel --depth 1
+#   cd WSL2-Linux-Kernel/tools/perf
+#   make -j$(nproc)
