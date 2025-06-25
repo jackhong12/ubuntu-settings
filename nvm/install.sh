894b48803133dc2bfff09c9fa0112a0ec25bd87c
Zhen-Hong Huang <zjackhua@qti.qualcomm.com>
2025-06-25 15:33:58 +0800

    Link nvm folder to ~/.zsh/ubuntu-setting/nvm

--- nvm/install.sh
+++ nvm/install.sh
@@ -4,7 +4,7 @@ source ../zsh/scripts/utils.zsh
 
 prun curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-prun ln -sf `pwd`/nvm.zsh ~/.zsh
+prun ln -sf `pwd`/nvm.zsh ~/.zsh/ubuntu-settings-nvm
 
 # Install Node.js 20
 prun source nvm.zsh
