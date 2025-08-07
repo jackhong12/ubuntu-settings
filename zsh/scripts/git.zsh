f3b1734e6f0ee0716a8e0d722ff3d90e2369ff70
zjackhua <zjackhua@qti.qualcomm.com>
2025-08-07 17:01:05 +0800

    Add git-log-pretty

--- zsh/scripts/git.zsh
+++ zsh/scripts/git.zsh
@@ -1,5 +1,22 @@
+#!/bin/zsh
 
 # Create a new branch
 #   $ git checkout -b <branch>
 # Switch to a branch
 #   $ git checkout <branch>
+# Avoid tracking files
+#   $ git update-index --assume-unchanged <file>
+# Change the commit message without modifying the content
+#   $ git commit --amend
+
+# include utils.zsh {{{
+if [ -f ~/.zsh/utils.zsh ]; then
+  source ~/.zsh/utils.zsh
+else
+  printf "\033[0;31mNot file ~/.zsh/utils.zsh\033[0m\n"
+fi
+# }}} include utils.zsh
+
+git-log-pretty () {
+  git log --graph --decorate --pretty=format:"%C(auto)%h %Cblue%cd%C(auto)%d %s %Cgreen%an" --date=format:"%m/%d"
+}
