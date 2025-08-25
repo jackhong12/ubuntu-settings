c38ec642363cab23526dbbd97b22aee3c648b5aa
zjackhua <zjackhua@qti.qualcomm.com>
2025-08-25 15:38:39 +0800

    Add ln_sf_check to create non-existing soft link

---
+++ zsh/scripts/ln.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# include utils.zsh {{{
+if [ -f ~/.zsh/utils.zsh ]; then
+  source ~/.zsh/utils.zsh
+else
+  printf "\033[0;31mNot file ~/.zsh/utils.zsh\033[0m\n"
+fi
+# }}} include utils.zsh
+
+
+# ln_sf_check: Create a symbolic link if it doesn't exist. {{{
+ln_sf_check () {
+  # If the arguments are smaller than 2, return 1.
+  if [ $# -lt 2 ]; then
+    return 1
+  fi
+
+  # If the target file has already been linked, return 0.
+  if [ -L "$2" ] && [ "$(readlink "$2")" = "$1" ]; then
+    return 0
+  fi
+
+  prun ln -sf "$1" "$2"
+}
+# }}} ln_sf_check
