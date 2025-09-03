b72bcdaca37babe31695f5a36f5ae772c81c42da
zjackhua <zjackhua@qti.qualcomm.com>
2025-09-03 16:09:01 +0800

    Add docker_run_bash to run a image

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,10 +1,7 @@
 source ~/.zsh/zlib.zsh
 zinclude "prun.zsh"
 
-
 DIR=$(zlib_repo_path)/docker
-DOCKERFILE=$DIR/Dockerfile.lite
-IMG_LITE=ubuntu20_lite
 
 # dls {{{
 # show all docker image
@@ -16,14 +13,15 @@ dls () {
 
 # docker_build_current_folder: Build docker image accroding Dockerfile under current folder {{{
 
+# TODO: use buildx instead.
 docker_build_current_folder () {
-  current_folder=${`pwd`:t}
+  current_folder=${PWD:t}
   prun docker build .                   \
-    --file $DOCKERFILE                  \
     --build-arg usern=$USER             \
     --build-arg USER_UID=$(id -g $USER) \
     --build-arg USER_GID=$(id -g $USER) \
-    -t $current_folder
+    -t $current_folder                  \
+    $@
 }
 
 # }}} docker_build_current_folder
@@ -42,21 +40,25 @@ drm () {
 
 #}}} drm
 
-# drunlite {{{
+# docker_run_bash: Run a bash in docker with --rm option {{{
 
-drunlite () {
-  img=$IMG_LITE
-  docker run                                         \
+docker_run_bash () {
+  img=$1
+  current_folder=${PWD:t}
+  mkdir -p docker_history
+  touch docker_history/.bash_history
+  prun docker run                                    \
     -it                                              \
     --rm                                             \
     -e "TERM=xterm-256color"                         \
     -e DISPLAY=$DISPLAY                              \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/               \
-    -v $(pwd):/home/jack/share                       \
+    -v $(pwd)/docker_history/:/home/$USER/share      \
+    -v $(pwd):/home/$USER/$current_folder            \
     --network host                                   \
     --detach-keys="ctrl-@"                           \
     --privileged=true                                \
     $img                                             \
     bash
 }
-#}}} drunlite
+#}}} docker_run_bash
